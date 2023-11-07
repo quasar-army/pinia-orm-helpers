b93c24f74a4fb3f68276a86e07807e0d26cb9a93
Luke Diebold <luke@ldiebold.com>
2023-11-07 10:11:26 +1000

    build process improvements

---
+++ dist/index.cjs
@@ -0,0 +1,46 @@
+'use strict';
+
+const piniaOrm = require('pinia-orm');
+
+function getClassAttributes(PiniaOrmClass) {
+  const fields = new PiniaOrmClass().$fields();
+  return Object.fromEntries(Object.entries(fields).map(([key, schema]) => {
+    const definition = {
+      ...schema,
+      kind: schema.constructor.name,
+      isRelationship: schema instanceof piniaOrm.Relation,
+      key
+    };
+    return [key, definition];
+  }).filter(([_key, schema]) => typeof schema === "object" ? !schema.isRelationship : false));
+}
+
+function getClassFields(PiniaOrmClass) {
+  const fields = new PiniaOrmClass().$fields();
+  return Object.fromEntries(Object.entries(fields).map(([key, schema]) => {
+    const definition = {
+      ...schema,
+      kind: schema.constructor.name,
+      isRelationship: schema instanceof piniaOrm.Relation,
+      key
+    };
+    return [key, definition];
+  }));
+}
+
+function getClassRelationships(PiniaOrmClass) {
+  const fields = new PiniaOrmClass().$fields();
+  return Object.fromEntries(Object.entries(fields).map(([key, schema]) => {
+    const definition = {
+      ...schema,
+      kind: schema.constructor.name,
+      isRelationship: schema instanceof piniaOrm.Relation,
+      key
+    };
+    return [key, definition];
+  }).filter(([_key, schema]) => typeof schema === "object" ? !schema.isRelationship : false));
+}
+
+exports.getClassAttributes = getClassAttributes;
+exports.getClassFields = getClassFields;
+exports.getClassRelationships = getClassRelationships;
